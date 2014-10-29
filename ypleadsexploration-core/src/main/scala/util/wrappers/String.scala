5a4fd88a9354cfb5e0809213fc51ac323893d2f9
Luis Da Costa <dacosta.le@gmail.com>
2014-10-29 15:09:08 -0400

    'CleanStringImpl' is private[this]

--- ypleadsexploration-core/src/main/scala/util/wrappers/String.scala
+++ ypleadsexploration-core/src/main/scala/util/wrappers/String.scala
@@ -17,7 +17,7 @@ object String {
   trait Clean extends Wrapper
   object Clean extends Serializable {
     def apply(s: String): Clean = { CleanStringImpl(s.replace("\"", "").trim) }
-    private case class CleanStringImpl(val value: String) extends Clean
+    private[this] case class CleanStringImpl(val value: String) extends Clean
   }
 
   implicit def string2Clean(s: String): Clean = Clean(s)
